5f9d1700e2596991d939790b908f7b204e0c326e
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-12-21 12:44:56 +0530

    using state properties

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // all components, whether functional or class based, have a special property called state
+  // this state helps to manage data from within the component unlike supplying data from outside the component
+  state = {
+    persons: [
+      {name: 'Rishabh 1', age: 20},
+      {name: 'Rishabh 2', age: 30},
+      {name: 'Rishabh 3', age: 40}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi I am a react application</h1>
-        <Person  name="Rishabh" age="28"/>
-        <Person  name="Rishabh 1" age="29">My Hobbies are Playing Golf</Person>
-        <Person  name="Rishabh 2" age="30"/>
+        <Person  name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person  name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies are Playing Golf</Person>
+        <Person  name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button>Click to Switch</button>
       </div>
     );
   }
