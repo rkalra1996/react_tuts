bb1a6c3c0b7d050b43d767538f505d6f0a8357db
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-12-27 21:56:19 +0530

    controlling useEffect behaviour

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -7,8 +7,16 @@ const Cockpit = (props) => {
         // this means it will be called whenever a connected update functionality is triggered
         // this will also get triggered the first time component is created
         // this REACT HOOK is usefull to perform side effect functionality
+
+        // The issue is its feature, to get triggered onevery render
+        // but we can control it too.....
+        // Example, this function will be executed only when persons variable changes
+        // Note that it WILL TRIGGER the first time as it is the default behaviour
+        setTimeout(() => {
+            alert('hi');
+        }, 1000)
         console.log('[Cockpit.js] useEffect');
-    });
+    }, [props.persons]);
     console.log('[Cockpit.js] render')
     return (
         <div>
