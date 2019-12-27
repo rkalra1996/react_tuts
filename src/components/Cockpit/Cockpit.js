1c9f74a4d667b39f3bc46064b5ac991640f72e8b
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-12-27 21:49:39 +0530

    using useEffect REACT HOOK to mimic lifecycle hooks of class components, in functional components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import classes from './Cockpit.module.css';
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+    useEffect(() => {
+        // this basically gets called on every render cycle of functional component
+        // this means it will be called whenever a connected update functionality is triggered
+        // this will also get triggered the first time component is created
+        // this REACT HOOK is usefull to perform side effect functionality
+        console.log('[Cockpit.js] useEffect');
+    });
     console.log('[Cockpit.js] render')
     return (
         <div>
@@ -13,4 +20,4 @@ const cockpit = (props) => {
     )
 }
 
-export default cockpit;
+export default Cockpit;
